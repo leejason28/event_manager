52ccabf1f718ea8aa9287797ebe26cfe5582d4ee
leejason28 <jason928lee@gmail.com>
2023-05-13 13:23:32 -0700

    time targeting

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -46,6 +46,37 @@ def clean_phone_number(phone_number)
   end
 end
 
+def peak_hour(times)
+  times.max_by { |i| times.count(i) }
+end
+
+def clean_time(date_time)
+  if date_time[0].length < 8
+    date_array = date_time[0].split("/")
+    for i in 0..1
+      if date_array[i].length < 2
+        date_array[i] = "0" + date_array[i]
+      end
+    end
+    date_time[0] = date_array.join("/")
+  end
+  year = ("20" + date_time[0][-2..-1]).to_i
+  month = date_time[0][0..1]
+  date = date_time[0][3..4]
+
+  if date_time[1].length < 5
+    time_array = date_time[1].split(":")
+    if time_array[0].length < 2
+      time_array[0] = "0" + time_array[0]
+    end
+    date_time[1] = time_array.join(":")
+  end
+  hour = date_time[1][0..1]
+  min = date_time[1][3..4]
+
+  return Time.new(year,month,date,hour,min)
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -56,6 +87,8 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+
+times = []
 
 contents.each do |row| #commented shit out so its faster when i do assignments
   #id = row[0]
@@ -69,5 +102,13 @@ contents.each do |row| #commented shit out so its faster when i do assignments
 
   #phone_number = clean_phone_number(row[:homephone].to_s.gsub(/\p{^Alnum}/, ""))
 
-  puts "#{phone_number}"
+  cleaned_time = clean_time(row[:regdate].split(" "))
+  times.push(cleaned_time)
+
 end
+
+hours = []
+times.each do |time|
+  hours.push(time.hour)
+end
+puts "#{peak_hour(hours)}"
